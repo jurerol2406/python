99826e1389b68705bfcc65c61d2c169f380bad32
jurerol2406 <jurerol2406@ieszaidinvergeles.org>
2025-09-25 11:52:59 +0200

    area+perimetro

--- entradasalida.py
+++ entradasalida.py
@@ -1,6 +1,15 @@
-num1=int(input("Primer número a sumar:"))
-print(num1)
-num2=int(input("Segundo número a sumar:"))
-print(num2)
+#Sumas
+#num1=int(input("Primer número a sumar:"))
+#print(num1)
+#num2=int(input("Segundo número a sumar:"))
+#print(num2)
 
-print(num1 + num2)
+#print(num1 + num2)
+
+
+#RECTANGULO
+base=float(input("Di la base del rectángulo:"))
+altura=float(input("Di la altura del rectángulo"))
+
+print("El Área del Rectángulo es: " + str(base*altura)  )
+print("El Perimetro del Rectángulo es: " + str(base*2+altura*2) )

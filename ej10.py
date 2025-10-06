65509fe682dbaec085bdee8e1c35912db77dddb1
jurerol2406 <jurerol2406@ieszaidinvergeles.org>
2025-10-06 12:20:54 +0200

    ejercicio10

---
+++ ej10.py
@@ -0,0 +1,4 @@
+payasos = int(input("¿Cuántos payasos se han vendido en el ultimo pedido?"))
+muñecas = int(input("¿Cuántas muñecas se han vendido en el ultimo pedido?"))
+
+print ("El peso total del paquete es de "+str((payasos*112+muñecas*75)/1000))

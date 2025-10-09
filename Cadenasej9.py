d2607b3054943b8a653d0eedb45d40cae7a11617
jurerol2406 <jurerol2406@ieszaidinvergeles.org>
2025-10-09 10:56:06 +0200

    Ej9String

---
+++ Cadenasej9.py
@@ -0,0 +1,7 @@
+fecha = input ("Escribe tu fecha de nacimiento en formato dd/mm/aaaa: ")
+split = fecha.split("/")
+dia = split [0]
+mes = split [1]
+anio = split [2]
+
+print ("Naciste el dia "+ dia+ " del mes "+ mes + " del año "+ anio )

0dc8753cabbf8aa6edd17c426a4e0fcf338a5f35
jurerol2406 <jurerol2406@ieszaidinvergeles.org>
2025-10-16 09:53:50 +0200

    Ej3IF

---
+++ Cond3.py
@@ -0,0 +1,7 @@
+num1 = int(input("Di un numero: "))
+num2 = int(input("Di un numero: "))
+
+if num2 == 0:
+    print ("ERROR, divisor no puede ser 0")
+else:
+    print ("La división entre los números es: ", str(num1/num2))

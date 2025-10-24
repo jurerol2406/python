9ea426942504eb28ef05e498c0aafdc5daa73925
jurerol2406 <jurerol2406@ieszaidinvergeles.org>
2025-10-24 14:37:36 +0200

    Listas3CORREGIDO

--- LISTAS Y TUPLAS/Ejercicio3.py
+++ LISTAS Y TUPLAS/Ejercicio3.py
@@ -1,6 +1,9 @@
 asignaturas = ("Matematicas", "Lengua", "Fisica", "Quimica", "Historia")
 a = 0
+notas = []
 while a < len(asignaturas):
     nota = input("Que nota has sacado en: "+ asignaturas[a]+" ")
-    print ("En ", asignaturas[a], " has sacado un: ", nota)
+    notas.append(nota)
     a=a+1
+for i in range(len(asignaturas)):
+    print ("En ", asignaturas[i], " has sacado un: ", notas[i])
---
+++ LISTAS Y TUPLAS/Ejercicio4.py
@@ -0,0 +1,6 @@
+loteria = input ("Di los números de la loteria separados por COMAS (,): ")
+lista = loteria.split(",")
+
+a = 0
+while a < len(lista):
+    

45b06c60e31cde0fc06b7dd90dabaa85f55c2ca6
jurerol2406 <jurerol2406@ieszaidinvergeles.org>
2025-11-06 10:05:05 +0100

    Diccionario2

---
+++ DICCIONARIO/Ejercicio2.py
@@ -0,0 +1,8 @@
+dicc=dict(
+    nombre=input("Di tu nombre: "),
+    edad=input("Di tu edad: "),
+    dire=input("Di tu direccion: "),
+    tel=input ("Di tu telefono: ")
+)
+
+print (dicc["nombre"], " tiene ", dicc["edad"], " años, vive en ", dicc["dire"], " y su número de telefono es ", dicc["tel"])
